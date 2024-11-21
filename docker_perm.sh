39e73dddac1347671c798d9eeda59f418977edb5
SATYA_N_B <satyastudy24@gmail.com>
2024-11-21 08:15:15 +0530

    Update docker_perm.sh

--- docker_perm.sh
+++ docker_perm.sh
@@ -1,5 +1,18 @@
+#!/bin/bash
+
 sudo usermod -aG docker jenkins
+sleep 3
+
 chmod 777 /var/run/docker.sock
+sleep 3
+
 systemctl daemon-reload
+sleep 3
+
 systemctl restart jenkins
+sleep 5
+
 systemctl restart docker
+sleep 3
+
+echo "Jenkins and Docker services have been restarted, and the necessary configurations are applied."
